124df76d49883105239cd51f0f1006f88a4e3aa9
yqianjiang <y.jiang.psy@gmail.com>
2025-04-13 01:37:12 +0800

    更换图标

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1744477520|124751342549';
+const CACHE_VERSION = '1744479250|126477342435';
 /** @type {string} */
 const CACHE_PREFIX = 'Tetris-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
